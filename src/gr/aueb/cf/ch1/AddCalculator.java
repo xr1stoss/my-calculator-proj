7a856be8cfbb07f7170aa641e6962a5413153d83
Xristos Mpertes <xristosster@yahoo.gr>
2025-01-14 15:33:05 +0200

    project created

---
+++ src/gr/aueb/cf/ch1/AddCalculator.java
@@ -0,0 +1,16 @@
+package gr.aueb.cf.ch1;
+
+public class AddCalculator {
+
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int result = 0;
+
+        result = add(num1, num2);
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
